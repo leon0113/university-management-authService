970181dac8ed01d22d790322a980b5125ff2a5de
Leon <tahjib.leon399@gmail.com>
2023-08-09 13:34:10 +0600

    add cors and parser

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
 import express, { Application } from 'express';
+import cors from 'cors';
+
 const app: Application = express()
-// const port = 3000
+
+app.use(cors())
+
+//parser 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
