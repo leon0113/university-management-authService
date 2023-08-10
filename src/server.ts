c27ac997cecb6f9442a0c5a968048fd53d19931b
Leon <tahjib.leon399@gmail.com>
2023-08-10 14:59:16 +0600

    testing

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
-import mongoose from "mongoose";
-import app from './app';
+import mongoose from 'mongoose'
+import app from './app'
 // import port from './confiq/index';
-import confiq from "./confiq/index";
+import confiq from './confiq/index'
 
 async function boostrap() {
   try {
-    await mongoose.connect(confiq.database_url as string);
-    console.log(`✌ MongoDB connection established successfully.`);
+    await mongoose.connect(confiq.database_url as string)
+    console.log(`✌ MongoDB connection established successfully.`)
 
     app.listen(confiq.port, () => {
       console.log(`Application listening on port ${confiq.port}`)
     })
   } catch (err) {
-    console.log('Failed to connect database!', err);
+    console.log('Failed to connect database!', err)
   }
 }
 
-boostrap();
+boostrap()
