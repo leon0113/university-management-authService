05b317d5f6222b75adb449e7b64100dcef11d2d9
Leon <tahjib.leon399@gmail.com>
2023-08-25 05:11:46 +0600

    Student Module DONE

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { SortOrder } from 'mongoose';
 import { paginationHelper } from '../../../helpers/paginationHelper';
 import { IGenericPaginationResponse } from '../../../interfaces/common';
@@ -5,6 +6,8 @@ import { IPaginationOptions } from '../../../interfaces/paginationType';
 import { IStudent, IStudentFilterOption } from './student.interface';
 import { Student } from './student.model';
 import { studentSearchableFields } from './student.constant';
+import ApiError from '../../../errors/ApiError';
+import httpStatus from 'http-status';
 
 //! pagination search and filltering
 const getAllStudents = async (
@@ -68,7 +71,8 @@ const getAllStudents = async (
   };
 };
 
-// Get single semster
+// Get single studentw
+
 const getSingleStudent = async (id: string): Promise<IStudent | null> => {
   const result = await Student.findById(id)
     .populate('academicSemester')
@@ -77,21 +81,55 @@ const getSingleStudent = async (id: string): Promise<IStudent | null> => {
   return result;
 };
 
-// to update a semester
-// const updateStudent = async (
-//     id: string,
-//     payload: Partial<IStudent>,
-// ): Promise<IStudent | null> => {
+// to update a student
+const updateStudent = async (
+  id: string,
+  payload: Partial<IStudent>,
+): Promise<IStudent | null> => {
+  const isExist = await Student.findOne({ id });
 
-//     const result = await Student.findOneAndUpdate({ _id: id }, payload, {
-//         new: true,
-//     });
-//     return result;
-// };
+  if (!isExist) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Student not found');
+  }
 
-// delete semester
+  const { name, guardian, localGuardian, ...studentData } = payload;
+
+  const updatedStudentData: Partial<IStudent | any> = { ...studentData };
+
+  // name dynamically handling
+  if (name && Object.keys(name).length > 0) {
+    Object.keys(name).forEach(key => {
+      const nameKey = `name.${key}`; // `name.firstName`   `name.middleName` `name.lastName`
+      updatedStudentData[nameKey as keyof Partial<IStudent>] =
+        name[key as keyof typeof name];
+    });
+  }
+  // guardian dynamically handling
+  if (guardian && Object.keys(guardian).length > 0) {
+    Object.keys(guardian).forEach(key => {
+      const guardianKey = `guardian.${key}`;
+      updatedStudentData[guardianKey as keyof Partial<IStudent>] =
+        guardian[key as keyof typeof guardian];
+    });
+  }
+  // localGuardian dynamically handling
+  if (localGuardian && Object.keys(localGuardian).length > 0) {
+    Object.keys(localGuardian).forEach(key => {
+      const localGuardianKey = `localGuardian.${key}`;
+      updatedStudentData[localGuardianKey as keyof Partial<IStudent>] =
+        localGuardian[key as keyof typeof localGuardian];
+    });
+  }
+
+  const result = await Student.findOneAndUpdate({ id }, updatedStudentData, {
+    new: true,
+  });
+  return result;
+};
+
+// delete student
 const deleteStudent = async (id: string): Promise<IStudent | null> => {
-  const result = await Student.findByIdAndDelete({ _id: id })
+  const result = await Student.findByIdAndDelete({ id })
     .populate('academicSemester')
     .populate('academicDepartment')
     .populate('academicFaculty');
@@ -101,6 +139,6 @@ const deleteStudent = async (id: string): Promise<IStudent | null> => {
 export const StudentService = {
   getAllStudents,
   getSingleStudent,
-  // updateStudent,
+  updateStudent,
   deleteStudent,
 };
